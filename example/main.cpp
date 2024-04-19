47c6a0f139737da315cb3146c5d08c55d950e0ad
LegendWasTaken <caiodcw@gmail.com>
2024-04-20 06:20:01 +0800

    add ping packet to example

--- example/main.cpp
+++ example/main.cpp
@@ -24,6 +24,9 @@ void handle_status_response(std::string json_response) {
     std::cout << "Status response: " << json_response << std::endl;
 }
 
+void handle_ping_response(std::uint64_t payload) {
+    std::cout << "Ping response: " << payload << std::endl;
+}
 int main() {
     std::cout << "Connecting to localhost" << std::endl;
     auto connection = connect_to_localhost();
@@ -32,21 +35,27 @@ int main() {
     auto network_state = mcp::basic_network_state<void>();
 
     auto bytes = protocol::serialize<mcp::handshake_s>(network_state,
-                                                             765,
-                                                             "localhost",
-                                                             25565,
-                                                             1);
+                                                       765,
+                                                       "localhost",
+                                                       25565,
+                                                       1);
     connection.write(bytes.data(), bytes.size());
+
+    auto deserializer = protocol::deserializer<
+            mcp::status_status_response_c<handle_status_response>,
+            mcp::status_ping_response_c<handle_ping_response>>();
+
+    auto read_buffer = std::vector<std::byte>(1024);
 
     bytes = protocol::serialize<mcp::status_status_request_s>(network_state);
     connection.write(bytes.data(), bytes.size());
-
     std::this_thread::sleep_for(std::chrono::seconds(1));
+    connection.read(read_buffer.data(), read_buffer.size());
+    deserializer.decode(network_state, read_buffer);
 
-    auto deserializer = protocol::deserializer<
-            mcp::status_status_response_c<handle_status_response>>();
-
-    auto read_buffer = std::vector<std::byte>(1024);
+    bytes = protocol::serialize<mcp::status_ping_request_s>(network_state, 123456789);
+    connection.write(bytes.data(), bytes.size());
+    std::this_thread::sleep_for(std::chrono::seconds(1));
     connection.read(read_buffer.data(), read_buffer.size());
     deserializer.decode(network_state, read_buffer);
 
