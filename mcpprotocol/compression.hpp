d295f79c7e654fbc009d0f4ed207bc47a99ce4a5
wffirilat <wffirilat@users.noreply.github.com>
2024-04-21 14:52:13 -0500

    Add theoretically-working compression code (cmake/linker is sad about it)

--- mcpprotocol/compression.hpp
+++ mcpprotocol/compression.hpp
@@ -1,6 +1,8 @@
 #ifndef MCPPROTOCOL_COMPRESSION_HPP
 #define MCPPROTOCOL_COMPRESSION_HPP
 
+#include <zlib.h>
+
 namespace mcp {
     inline std::vector<std::byte> TEMP_reverse(std::span<const std::byte> compressed_data) {
         // Promote span to vector using constructor
@@ -13,13 +15,66 @@ namespace mcp {
     }
 
     inline std::vector<std::byte> decompress(std::span<const std::byte> compressed_data, uint32_t size_hint) {
-        return TEMP_reverse(compressed_data);
-        throw std::runtime_error("TODO: decompression"); // TODO: decompression
+        zlibVersion();
+        uLongf /*mut*/ length = size_hint;
+        auto *buffer = new std::byte[length];
+
+        int errcode = uncompress(reinterpret_cast<Bytef *>(buffer), &/*mut*/ length,
+                                 reinterpret_cast<const Bytef *>(compressed_data.data()), compressed_data.size());
+
+        if (errcode != Z_OK) {
+            delete[] buffer;
+            switch (errcode) {
+                case Z_MEM_ERROR:
+                    throw std::runtime_error("out of memory for decompression, kill me now");
+                case Z_BUF_ERROR:
+                    throw std::runtime_error("received bad size for decompression (buffer too small)");
+                case Z_DATA_ERROR:
+                    throw std::runtime_error("received bad data for decompression (data corrupted or incomplete)");
+                default:
+                    throw std::runtime_error("zlib threw an undocumented error (unreachable hopefully)");
+            }
+        }
+        if (length != size_hint) {
+            delete[] buffer;
+            throw std::runtime_error("received bad size for decompression (buffer too large)");
+        }
+
+        auto result = std::vector<std::byte>(buffer, buffer + length);
+
+        delete[] buffer;
+
+        return result;
     }
 
     inline std::vector<std::byte> compress(std::span<const std::byte> uncompressed_data) {
-        return TEMP_reverse(uncompressed_data);
-        throw std::runtime_error("TODO: decompression"); // TODO: decompression
+        uLong /*mut*/ length = compressBound(uncompressed_data.size());
+        auto *buffer = new std::byte[length];
+
+        int errcode = compress2(reinterpret_cast<Bytef *>(buffer), &/*mut*/ length,
+                                reinterpret_cast<const Bytef *>(uncompressed_data.data()),
+                                uncompressed_data.size_bytes(),
+                                Z_DEFAULT_COMPRESSION);
+
+        if (errcode != Z_OK) {
+            delete[] buffer;
+            switch (errcode) {
+                case Z_MEM_ERROR:
+                    throw std::runtime_error("out of memory for compression, kill me now");
+                case Z_BUF_ERROR:
+                    throw std::runtime_error("zlib::compressBound lied to us (not enough space in buffer)");
+                case Z_STREAM_ERROR:
+                    throw std::runtime_error("zlib rejected its own default compression level (invalid compression level)");
+                default:
+                    throw std::runtime_error("zlib threw an undocumented error (unreachable hopefully)");
+            }
+        }
+
+        auto result = std::vector<std::byte>(buffer, buffer + length);
+
+        delete[] buffer;
+
+        return result;
     }
 }
 #endif //MCPPROTOCOL_COMPRESSION_HPP
